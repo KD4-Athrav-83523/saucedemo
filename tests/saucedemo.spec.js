eed8d044848eaa17775e9b084e2ae5069fb57f20
unknown <apailwan53@gmail.com>
2026-08-19 13:23:44 +0530

    pushing the code from saucedemo website

---
+++ tests/saucedemo.spec.js
@@ -0,0 +1,32 @@
+import { test } from '@playwright/test';
+
+test("@Regression End to End flow", async({page})=>{
+
+    await page.goto("https://www.saucedemo.com/");
+
+    await page.locator("#user-name").fill("standard_user");
+
+    await page.locator("#password").fill("secret_sauce");
+
+    await page.waitForTimeout(3000);
+
+    await page.locator("#login-button").click();
+
+    await page.waitForTimeout(3000);
+
+    await page.locator(".product_sort_container").selectOption({label : 'Price (high to low)'});
+
+    await page.waitForTimeout(3000);
+
+    await page.locator("button[name='add-to-cart-sauce-labs-fleece-jacket']").click();
+
+    await page.waitForTimeout(3000);
+
+    //used to locate single elements in HTML DOM Structure
+    const container = await page.$("//span[@class='shopping_cart_badge']");
+
+    await container.click();
+
+    await page.waitForTimeout(3000);
+
+});
